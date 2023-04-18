52fb44b19ae13b274c85b06bf33ecffc50adde63
Hector Lecona Cardenas <hector.cardenas@impacx.de>
2023-04-18 09:34:38 +0200

    template vue

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <h1> {{message}} </h1>
+	<h2> {{message_two}} </h2>
+  
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      message: 'DINX 1',
+      message_two: 'Perlo 2',
+
+    }
+  }
+
+}
+</script>
+
+
+
+<style scoped>
+body{
+
+  box-sizing: border-box;
+  
+}
+
+.spacer{
+  margin-top: 100px;
+}
+
+.background {
+  background: rgb(176, 15, 98);
+  background: radial-gradient(circle, rgba(84, 98, 98, 0.286) 0%, rgb(254, 254, 254) 100%); 
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: -1;
+  box-sizing: border-box;
+  left: 0;
+  top: 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+//components
+// import HeaderPos from './components/HeaderPos.vue'
+
+
+
+const app = createApp(App);
+// app.component('header-pos', HeaderPos);
+
+app.mount('#app')
+
